617fed949401eea89f18f0f6356bcd1f40d20c86
Rachel Bowyer <rachbowyer@gmail.com>
2021-03-10 22:31:44 +0000

    Bring up to date with Java 11 and the latest version of keirin

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject keirin-demo "0.1.0-SNAPSHOT"
+(defproject keirin-demo "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :global-vars {*warn-on-reflection* true *assert* true}
+  :global-vars {*warn-on-reflection* false *assert* true}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [analemma "1.0.0-SNAPSHOT"]
-                 [keirin "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [analemma "1.1.0"]
+                 [rachbowyer/keirin "1.1.1"]]
 
   :main ^:skip-aot keirin-demo.core
   :target-path "target/%s"
-  :profiles {:dev {:jvm-opts ^:replace ["-Xms4g" "-Xmx4g" "-server" "-Xloggc:gc.out" "-XX:-TieredCompilation" "-Xbatch" "-XX:CICompilerCount=1"]}
+  :profiles {:dev {:jvm-opts ^:replace ["-Xms4g" "-Xmx4g" "-server" "-Xlog:gc:gc.out" "-XX:-TieredCompilation" "-Xbatch" "-XX:CICompilerCount=1"]}
              :uberjar {:jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]
                        :aot :all}})
 
